51eb1e739e9357d90a65424c688932dea123c81f
Piyal Guho Dipto <diptoguho4@gmail.com>
2024-11-23 03:29:35 +0600

    question form

--- src/components/admin/quiz/QuestionForm.jsx
+++ src/components/admin/quiz/QuestionForm.jsx
@@ -1,109 +1,164 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import useAxios from "../../../hooks/useAxios";
+import { useQuiz } from "../../../hooks/useQuiz";
+import Field from "../../common/Field";
+
 export default function QuestionForm() {
+  const { api } = useAxios();
+  const [correctAnswer, setCorrectAnswer] = useState("");
+  //   const navigate = useNavigat();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm();
+  const { quiz, setQuiz } = useQuiz();
+  function questionSubmit(formData) {
+    const options = [
+      formData.option1,
+      formData.option2,
+      formData.option3,
+      formData.option4,
+    ];
+
+    const questionPayload = {
+      question: formData.question,
+      options,
+      correctAnswer, // চেকড ভ্যালু স্টেট থেকে যোগ করা হবে।
+    };
+    console.log(questionPayload);
+  }
   return (
     <>
-    <form>
-      <div>
-        <label
-          htmlFor="quizTitle"
-          className="block text-sm font-medium text-foreground mb-1"
-        >
-          Question Title
-        </label>
-        <input
-          type="text"
-          id="quizTitle"
-          name="quizTitle"
-          className="w-full mt-2 p-2 border border-input rounded-md bg-background text-foreground"
-          placeholder="Enter quiz title"
-        />
-      </div>
-
-      <p className="text-sm text-gray-600 mt-4">Add Options</p>
-
-      <div id="optionsContainer" className="space-y-2 mt-4">
-        <div className="flex items-center space-x-2 px-4 py-1 rounded-md group focus-within:ring focus-within:ring-primary/80 bg-white">
-          <input
-            type="checkbox"
-            id="option0"
-            name="correctAnswer"
-            value="0"
-            className="text-primary focus:ring-0 w-4 h-4"
-          />
-          <label htmlFor="option0" className="sr-only">
-            Option 1
-          </label>
-          <input
-            type="text"
-            id="optionText0"
-            name="optionText0"
-            className="w-full p-2 bg-transparent rounded-md text-foreground outline-none focus:ring-0"
-            placeholder="Option 1"
-          />
+      <form onSubmit={handleSubmit(questionSubmit)}>
+        <div>
+          <Field label="Question Title" error={errors.question}>
+            <input
+              {...register("question", {
+                required: "Question  title is required",
+              })}
+              className={`w-full mt-2 p-2 border border-input rounded-md bg-background text-foreground ${
+                errors.question ? "border-red-500" : "border-gray-200"
+              }`}
+              name="question"
+              type="text"
+              id="question"
+              placeholder="Enter Question title"
+            />
+          </Field>
         </div>
 
-        <div className="flex items-center space-x-2 px-4 py-1 rounded-md group focus-within:ring focus-within:ring-primary/80 bg-white">
-          <input
-            type="checkbox"
-            id="option2"
-            name="correctAnswer"
-            value="0"
-            className="text-primary focus:ring-0 w-4 h-4"
-          />
-          <label htmlFor="option0" className="sr-only">
-            Option 2
-          </label>
-          <input
-            type="text"
-            id="optionText2"
-            name="optionText2"
-            className="w-full p-2 bg-transparent rounded-md text-foreground outline-none focus:ring-0"
-            placeholder="Option 2"
-          />
-        </div>
+        <p className="text-sm text-gray-600 mt-4">Add Options</p>
 
-        <div className="flex items-center space-x-2 px-4 py-1 rounded-md group focus-within:ring focus-within:ring-primary/80 bg-white">
-          <input
-            type="checkbox"
-            id="option3"
-            name="correctAnswer"
-            value="0"
-            className="text-primary focus:ring-0 w-4 h-4"
-          />
-          <label htmlFor="option3" className="sr-only">
-            Option 3
-          </label>
-          <input
-            type="text"
-            id="optionText3"
-            name="optionText3"
-            className="w-full p-2 bg-transparent rounded-md text-foreground outline-none focus:ring-0"
-            placeholder="Option 3"
-          />
-        </div>
+        <div id="optionsContainer" className="space-y-2 mt-4">
+          <div className="flex items-center space-x-2 px-4 py-1 rounded-md group focus-within:ring focus-within:ring-primary/80 bg-white">
+            <input
+              {...register("correctAnswer")}
+              type="checkbox"
+              id="option1"
+              name="correctAnswer"
+              className="text-primary focus:ring-0 w-4 h-4"
+              onChange={(e) =>
+                setCorrectAnswer(e.target.checked ? e.target.value : "")
+              }
+            />
+            <Field className="sr-only" error={false}>
+              <input
+                {...register("option1", {
+                  required: "option1 is required",
+                })}
+                type="text"
+                id="option1"
+                name="option1"
+                className="w-full p-2 bg-transparent rounded-md text-foreground outline-none focus:ring-0"
+                placeholder="Option 1"
+              />
+            </Field>
+          </div>
 
-        <div className="flex items-center space-x-2 px-4 py-1 rounded-md group focus-within:ring focus-within:ring-primary/80 bg-white">
-          <input
-            type="checkbox"
-            id="option4"
-            name="correctAnswer"
-            value="0"
-            className="text-primary focus:ring-0 w-4 h-4"
-          />
-          <label htmlFor="option4" className="sr-only">
-            Option 4
-          </label>
-          <input
-            type="text"
-            id="optionText4"
-            name="optionText4"
-            className="w-full p-2 bg-transparent rounded-md text-foreground outline-none focus:ring-0"
-            placeholder="Option 4"
-          />
+          <div className="flex items-center space-x-2 px-4 py-1 rounded-md group focus-within:ring focus-within:ring-primary/80 bg-white">
+            <input
+              {...register("correctAnswer")}
+              type="checkbox"
+              id="option2"
+              name="correctAnswer"
+              value={correctAnswer}
+              className="text-primary focus:ring-0 w-4 h-4"
+              onChange={(e) =>
+                setCorrectAnswer(e.target.checked ? e.target.value : "")
+              }
+            />
+            <Field className="sr-only" error={false}>
+              <input
+                {...register("option2", {
+                  required: "option2 is required",
+                })}
+                type="text"
+                id="option2"
+                name="option2"
+                className="w-full p-2 bg-transparent rounded-md text-foreground outline-none focus:ring-0"
+                placeholder="Option 2"
+              />
+            </Field>
+          </div>
+
+          <div className="flex items-center space-x-2 px-4 py-1 rounded-md group focus-within:ring focus-within:ring-primary/80 bg-white">
+            <input
+              {...register("correctAnswer")}
+              type="checkbox"
+              id="option3"
+              name="correctAnswer"
+              value={correctAnswer}
+              className="text-primary focus:ring-0 w-4 h-4"
+              onChange={(e) =>
+                setCorrectAnswer(e.target.checked ? e.target.value : "")
+              }
+            />
+            <Field className="sr-only" error={false}>
+              <input
+                {...register("option3", {
+                  required: "option3 is required",
+                })}
+                type="text"
+                id="option3"
+                name="option3"
+                className="w-full p-2 bg-transparent rounded-md text-foreground outline-none focus:ring-0"
+                placeholder="Option 3"
+              />
+            </Field>
+          </div>
+
+          <div className="flex items-center space-x-2 px-4 py-1 rounded-md group focus-within:ring focus-within:ring-primary/80 bg-white">
+            <input
+              {...register("correctAnswer")}
+              type="checkbox"
+              id="option4"
+              name="correctAnswer"
+              value={correctAnswer}
+              className="text-primary focus:ring-0 w-4 h-4"
+              onChange={(e) =>
+                setCorrectAnswer(e.target.checked ? e.target.value : "")
+              }
+            />
+            <Field className="sr-only" error={false}>
+              <input
+                {...register("option4", {
+                  required: "option4 is required",
+                })}
+                type="text"
+                id="option4"
+                name="option4"
+                className="w-full p-2 bg-transparent rounded-md text-foreground outline-none focus:ring-0"
+                placeholder="Option 4"
+              />
+            </Field>
+          </div>
         </div>
-      </div>
-      <button className="w-full bg-primary text-white text-primary-foreground p-2 rounded-md hover:bg-primary/90 transition-colors">
-        Save Quiz
-      </button>
+        <button className="w-full bg-primary text-white text-primary-foreground p-2 rounded-md hover:bg-primary/90 transition-colors">
+          Save Quiz
+        </button>
       </form>
     </>
   );
