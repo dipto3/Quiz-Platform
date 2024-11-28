1e9be7047aa7364973adc20f7cdc6053ebf50cb2
Piyal Guho Dipto <diptoguho4@gmail.com>
2024-11-28 13:12:20 +0600

    question option get

--- src/components/frontend/Question.jsx
+++ src/components/frontend/Question.jsx
@@ -1,54 +1,33 @@
-export default function Question({question,onNext,isLast,onPrevious,isFirst}) {
+export default function Question({
+  question,
+  onNext,
+  isLast,
+  onPrevious,
+  isFirst,
+}) {
   return (
     <>
       <div className="lg:col-span-2 bg-white">
         <div className="bg-white p-6 !pb-2 rounded-md">
           <div className="flex justify-between items-center mb-4">
-            <h3 className="text-2xl font-semibold">
-              {question.question}
-            </h3>
+            <h3 className="text-2xl font-semibold">{question.question}</h3>
           </div>
           <div className="grid grid-cols-2 gap-4">
-            <label className="flex items-center space-x-3 py-3 px-4 bg-primary/5 rounded-md text-lg">
-              <input
-                type="checkbox"
-                name="answer1"
-                className="form-radio text-buzzr-purple"
-                checked
-              />
-              <span>0</span>
-            </label>
-
-            <label className="flex items-center space-x-3 py-3 px-4 bg-primary/5 rounded-md text-lg">
-              <input
-                type="checkbox"
-                name="answer2"
-                className="form-radio text-buzzr-purple"
-              />
-              <span>-1</span>
-            </label>
-
-            <label className="flex items-center space-x-3 py-3 px-4 bg-primary/5 rounded-md text-lg">
-              <input
-                type="checkbox"
-                name="answer3"
-                className="form-radio text-buzzr-purple"
-              />
-              <span>1</span>
-            </label>
-
-            <label className="flex items-center space-x-3 py-3 px-4 bg-primary/5 rounded-md text-lg">
-              <input
-                type="checkbox"
-                name="answer4"
-                className="form-radio text-buzzr-purple"
-              />
-              <span>1</span>
-            </label>
+            {question.options.map((option) => (
+              <label
+                key={option}
+                className="flex items-center space-x-3 py-3 px-4 bg-primary/5 rounded-md text-lg"
+              >
+                <input
+                  type="checkbox"
+                  name="answer1"
+                  className="form-radio text-buzzr-purple"
+                />
+                <span>{option}</span>
+              </label>
+            ))}
           </div>
-          <div className="flex justify-between items-center mt-6">
-            
-          </div>
+          <div className="flex justify-between items-center mt-6"></div>
           <button
             onClick={onPrevious}
             disabled={isFirst}
@@ -61,16 +40,16 @@ export default function Question({question,onNext,isLast,onPrevious,isFirst}) {
             {"<<"} Previous
           </button>
           <button
-          type="submit"
-          onClick={onNext}
-          disabled={isLast}
-          className={`w-1/2 ml-auto py-2 px-4 rounded-md ${
-            isLast
-              ? "bg-gray-400 text-white cursor-not-allowed"
-              : "bg-primary text-white hover:bg-indigo-800"
-          }`}
+            type="submit"
+            onClick={onNext}
+            disabled={isLast}
+            className={`w-1/2 ml-auto py-2 px-4 rounded-md ${
+              isLast
+                ? "bg-gray-400 text-white cursor-not-allowed"
+                : "bg-primary text-white hover:bg-indigo-800"
+            }`}
           >
-            Next  {">>"} 
+            Next {">>"}
           </button>
         </div>
       </div>
--- src/components/frontend/QuizPage.jsx
+++ src/components/frontend/QuizPage.jsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import useAxios from "../../hooks/useAxios";
 import Question from "./Question";
+import QuizDashboard from "./QuizDashboard";
 
 export default function QuizPage() {
   const { id } = useParams();
   const { api } = useAxios();
   const [questions, setQuestions] = useState([]);
+  const [quiz, setQuiz] = useState([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [error, setError] = useState(null);
   useEffect(() => {
@@ -16,6 +18,7 @@ export default function QuizPage() {
           `${import.meta.env.VITE_SERVER_BASE_URL}/quizzes/${id}`
         );
         setQuestions(response.data.data.questions);
+        setQuiz(response.data.data);
       } catch (error) {
         console.error(error);
         setError(error);
@@ -29,60 +32,30 @@ export default function QuizPage() {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
     }
   }
-  function handlePreviousQuestion () {
+  function handlePreviousQuestion() {
     if (currentQuestionIndex > 0) {
       setCurrentQuestionIndex(currentQuestionIndex - 1);
     }
-  };
+  }
   return (
     <>
       <main className="max-w-8xl mx-auto h-[calc(100vh-10rem)]">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 h-full">
-          <div className="lg:col-span-1 bg-white rounded-md p-6 h-full flex flex-col">
-            <div>
-              <h2 className="text-4xl font-bold mb-4">React Hooks Quiz</h2>
-              <p className="text-gray-600 mb-4">
-                A quiz on React hooks like useState, useEffect, and useContext.
-              </p>
-
-              <div className="flex flex-col">
-                <div className="w-fit bg-green-100 text-green-800 text-sm font-medium px-2.5 py-0.5 rounded-full inline-block mb-2">
-                  Total number of questions : 10
-                </div>
-
-                <div className="w-fit bg-blue-100 text-blue-800 text-sm font-medium px-2.5 py-0.5 rounded-full inline-block mb-2">
-                  Participation : 1
-                </div>
-
-                <div className="w-fit bg-gray-100 text-green-800 text-sm font-medium px-2.5 py-0.5 rounded-full inline-block mb-2">
-                  Remaining : 9
-                </div>
-              </div>
-            </div>
-
-            <div className="mt-auto flex items-center">
-              <img
-                src="./assets/avater.webp"
-                alt="Mr Hasan"
-                className="w-10 h-10 rounded-full mr-3 object-cover"
-              />
-              <span className="text-black font-semibold">Saad Hasan</span>
-            </div>
-          </div>
+          <QuizDashboard quiz={quiz} />
 
           {/* {questions.map((question) => ( */}
           {questions.length > 0 ? (
-          <Question
-            key={currentQuestionIndex}
-            question={questions[currentQuestionIndex]}
-            onNext={handleNextQuestion}
-            isLast={currentQuestionIndex === questions.length - 1}
-            isFirst={currentQuestionIndex === 0}
-            onPrevious={handlePreviousQuestion}
-          />
-        ) : (
-          <p>Loading questions...</p>
-        )}
+            <Question
+              key={currentQuestionIndex}
+              question={questions[currentQuestionIndex]}
+              onNext={handleNextQuestion}
+              isLast={currentQuestionIndex === questions.length - 1}
+              isFirst={currentQuestionIndex === 0}
+              onPrevious={handlePreviousQuestion}
+            />
+          ) : (
+            <p>Loading questions...</p>
+          )}
           {/* ))} */}
         </div>
       </main>
